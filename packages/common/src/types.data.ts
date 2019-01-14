cea1f9b1ee872d01f31619734f0457b93f88d470
Ankit Patel <1564233+gt3@users.noreply.github.com>
2019-01-13 22:31:41 -0800

    thunkable parent and child props

--- packages/common/src/types.data.ts
+++ packages/common/src/types.data.ts
@@ -4,15 +4,19 @@ export type EventData = SerializeableRecord<SerializeablePrimitives> & {
   eventName: string;
 };
 
-export type EventDataThunkable = {
-  eventName: string;
-  [key: string]: Serializable | ValueThunk<Serializable>;
-};
+export type EventDataThunkable =
+  | ValueThunk<EventData>
+  | {
+      eventName: string;
+      [key: string]: Serializable | ValueThunk<Serializable>;
+    };
 
 export type UserData = SerializeableRecord<SerializeablePrimitives> & {
   userId?: string | null;
 };
 
-export type UserDataThunkable = {
-  [key: string]: Serializable | ValueThunk<Serializable>;
-};
+export type UserDataThunkable =
+  | ValueThunk<UserData>
+  | {
+      [key: string]: Serializable | ValueThunk<Serializable>;
+    };
