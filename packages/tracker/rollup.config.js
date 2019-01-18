d495c040056e17be79a721ba5ba50e0c075a15f3
Ankit Patel <1564233+gt3@users.noreply.github.com>
2019-01-18 14:28:48 -0800

    use replace plugin for env

--- packages/tracker/rollup.config.js
+++ packages/tracker/rollup.config.js
@@ -1,6 +1,7 @@
 import nodeResolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import { terser } from 'rollup-plugin-terser'
+import replace from 'rollup-plugin-replace'
 
 import pkg from './package.json'
 
@@ -38,9 +39,9 @@ export default [
         exclude: 'node_modules/**',
         extensions: ['.ts', '.js']
       }),
-      // replace({
-      //   'process.env.NODE_ENV': JSON.stringify('production')
-      // }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
       terser({
         compress: {
           pure_getters: true,
