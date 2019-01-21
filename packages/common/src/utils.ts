32e410f5204a825ca744b88c72a9e525cff88f71
Ankit Patel <1564233+gt3@users.noreply.github.com>
2019-01-21 11:27:25 -0800

    simplet byte to hex conversion

--- packages/common/src/utils.ts
+++ packages/common/src/utils.ts
@@ -175,7 +175,12 @@ function toArrayBuffer(str: String): ArrayBuffer {
   return output.buffer;
 }
 
+const hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+
+function toHex(byte: number) {
+  return hex[(byte >> 4) & 0x0f] + hex[byte & 0x0f];
+}
+
 function toString(buffer: ArrayBuffer) {
-  // @ts-ignore
-  return String.fromCharCode.apply(null, new Uint8Array(buffer));
+  return Array.prototype.map.call(new Uint8Array(buffer), toHex).join('');
 }
