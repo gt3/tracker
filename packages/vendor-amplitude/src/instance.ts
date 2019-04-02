b7b435013fc3cedd6890fcce34af913e52606e52
Ankit Patel <1564233+gt3@users.noreply.github.com>
2019-04-02 14:57:17 -0700

    do the same for Amplitude API package

--- packages/vendor-amplitude/src/instance.ts
+++ packages/vendor-amplitude/src/instance.ts
@@ -1,6 +1,20 @@
-const _ctx = Function('return this;')();
+// todo: use context from utils (add dependency)
+const _ctx = getGlobal();
 
 export function getInstance<T>(vendorKey: string, projectKey?: string): T {
   const instance = _ctx[vendorKey];
   return instance && (projectKey ? instance.getInstance(projectKey) : instance.getInstance());
+}
+
+function getGlobal(): any {
+  if (typeof self !== 'undefined') {
+    return self;
+  }
+  if (typeof window !== 'undefined') {
+    return window;
+  }
+  if (typeof global !== 'undefined') {
+    return global;
+  }
+  throw new Error('unable to locate global object');
 }
